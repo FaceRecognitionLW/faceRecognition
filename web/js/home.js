0c99d114cc97486a8f1ee39e72948b0f78fd0adb
liuqiong <2876224574@qq.com>
2018-12-12 22:26:54 +0800

    home界面观察

--- web/js/home.js
+++ web/js/home.js
@@ -2,6 +2,7 @@
     var doc = document;
     function Home(){
         this.init();
+        this.position();
     }
     Object.defineProperty(Home.prototype,'constructor',{
         enumerable: false,
@@ -12,10 +13,20 @@
         init: function(){
             var oOpenAnimation = doc.getElementById('stage');
             var timer = setTimeout(function(){  
-                console.log(1);
                 oOpenAnimation.style.display = "none";
                 clearTimeout(timer);
             },2000)
+        },
+        position: function(){
+            console.log('position');
+            navigator.geolocation.getCurrentPosition(function(Position){
+                console.log('成功');
+                console.log('用户十进制纬度: '+Position.coords.latitude);
+                console.log('用户十进制经度: '+Position.coords.longitude)
+            },function(err){
+                console.log('Error code :'+err.code);
+                console.log('Erroe message: '+err.message)
+            })
         }
     };
     var home = new Home();
